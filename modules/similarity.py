8c175d4d046ab623fce03981e90d9c90f90d4e08
Dwi Aji Kurniawan <dwi.aji.kurniawan@gmail.com>
2017-02-12 11:34:03 +0700

    Add similarity indexx

---
+++ modules/similarity.py
@@ -0,0 +1,123 @@
+from math import *
+from decimal import Decimal
+
+def vector(a, b):
+    vec_a, vec_b = {}, {}
+    for token in set(a + b):
+        vec_a[token], vec_b[token] = 0, 0
+    for token in a:
+        vec_a[token] += 1
+    for token in b:
+        vec_b[token] += 1
+    return list(set(a + b)), vec_a, vec_b
+
+class Jaccard:
+    def unique(self, a):
+        return list(set(a))
+
+    def intersect(self, a, b):
+        return list(set(a) & set(b))
+
+    def union(self, a, b):
+        return list(set(a) | set(b))
+
+    def index(self, a = [], b = []):
+        try:
+            return len(self.intersect(a, b)) / len(self.union(a, b))
+        except ZeroDivisionError as e:
+            return 1
+
+class WeightedJaccard:
+    def index(self, a, b):
+        tokens, vec_a, vec_b = vector(a, b)
+        dividend = sum(list(map(lambda token: min(vec_a[token], vec_b[token]), tokens)))
+        divisor = sum(list(map(lambda token: max(vec_a[token], vec_b[token]), tokens)))
+        try:
+            return dividend / divisor
+        except ZeroDivisionError as e:
+            return 1
+
+class ExtendedJaccard:
+    def index(self, a, b):
+        tokens, vec_a, vec_b = vector(a, b)
+        sop = sum(list(map(lambda token: vec_a[token] * vec_b[token], tokens)))
+        dot_a = sum([value * value for attr, value in vec_a.items()])
+        dot_b = sum([value * value for attr, value in vec_b.items()])
+
+        divisor = dot_a + dot_b - sop
+        try:
+            return sop / divisor
+        except ZeroDivisionError as e:
+            return 1
+
+class Cosine:
+    def index(self, a, b):
+        tokens, vec_a, vec_b = vector(a, b)
+        # sum of product
+        sop = sum(list(map(lambda token: vec_a[token] * vec_b[token], tokens)))
+        # square root of sum of a square
+        sqrt_soas = sqrt(sum([value * value for attr, value in vec_a.items()]))
+        # square root of sum of b square
+        sqrt_sobs = sqrt(sum([value * value for attr, value in vec_b.items()]))
+        try:
+            return sop / (sqrt_soas * sqrt_sobs)
+        except ZeroDivisionError as e:
+            return 1
+
+class Dice:
+    def index(self, a, b):
+        tokens, vec_a, vec_b = vector(a, b)
+        sop = sum(list(map(lambda token: vec_a[token] * vec_b[token], tokens)))
+        dot_a = sum([value * value for attr, value in vec_a.items()])
+        dot_b = sum([value * value for attr, value in vec_b.items()])
+        try:
+            return (2 * sop) / (dot_a + dot_b)
+        except ZeroDivisionError as e:
+            return 1
+
+class Euclidean:
+    def index(self, a, b):
+        tokens, vec_a, vec_b = vector(a, b)
+        subtract = list(map(lambda token: vec_a[token] - vec_b[token], tokens))
+        sop = sum([e * e for e in subtract])
+        distance = sqrt(sop)
+        return 1 / (1 + distance)
+
+class Manhattan:
+    def index(self, a, b):
+        tokens, vec_a, vec_b = vector(a, b)
+        sum_of_subtract = sum(list(map(lambda token: vec_a[token] - vec_b[token], tokens)))
+        return 1 - (sum_of_subtract / len(tokens))
+
+# complicated
+# http://simeon.wikia.com/wiki/Minkowski_distance
+class Minkowski:
+    def index(self, a, b):
+        return 0
+
+# not suitable
+class Matching:
+    def index(self, a, b):
+        return 0
+
+class Overlap:
+    def intersect(self, a, b):
+        return list(set(a) & set(b))
+
+    def index(self, a = [], b = []):
+        try:
+            return len(self.intersect(a, b)) / min(len(a), len(b))
+        except ZeroDivisionError as e:
+            return 1
+
+class Pearson:
+    def index(self, a, b):
+        tokens, vec_a, vec_b = vector(a, b)
+        sop = sum(list(map(lambda token: vec_a[token] * vec_b[token], tokens)))
+        sum_of_a = sum([value for attr, value in vec_a.items()])
+        sum_of_b = sum([value for attr, value in vec_b.items()])
+        dividend = sop - ((sum_of_a * sum_of_b) / len(tokens))
+        sum_of_square_a = sum([value * value for attr, value in vec_a.items()])
+        sum_of_square_b = sum([value * value for attr, value in vec_b.items()])
+        divisor = sqrt((sum_of_square_a - (sum_of_a ** 2 / len(tokens))) * (sum_of_square_b - (sum_of_b ** 2 / len(tokens))))
+        return dividend / divisor
--- similarity_test_multi.py
+++ similarity_test_multi.py
@@ -7,20 +7,23 @@ import os
 import time as tm
 
 from modules import cleaner, tokenizer
-from modules.jaccard import Jaccard
-from modules.cosine import Cosine
+from modules.similarity import Jaccard, Cosine
 
 parser = argparse.ArgumentParser(description='Evaluate classifier model using ten folds cross validation.')
 parser.add_argument('-o', '--output', default='output.csv', help='File name for output CSV, e.g. output.csv')
-parser.add_argument('-a', '--algo', type=str, default='jaccard', help='Algorithm: jaccard, cosine')
 args = parser.parse_args()
 
-if args.algo == 'jaccard':
-    algo = Jaccard()
-elif args.algo == 'cosine':
-    algo = Cosine()
-else:
-    raise Exception('Algo not defined')
+calculations  = {
+    'jaccard': Jaccard(),
+    'cosine': Cosine(),
+    'weighted_jaccard': WeightedJaccard(),
+    'extended_jaccard': ExtendedJaccard(),
+    'dice': Dice(),
+    'manhattan': Manhattan(),
+    'euclidean': Euclidean(),
+    'overlap': Overlap(),
+    'pearson': Pearson()
+}
 
 with open(os.path.join(os.path.dirname(__file__), 'tweets_corpus/similarity_dataset_15028.csv'), newline='\n') as csv_input:
     dataset = csv.reader(csv_input, delimiter=',', quotechar='"')
@@ -28,77 +31,79 @@ with open(os.path.join(os.path.dirname(__file__), 'tweets_corpus/similarity_data
 
 def calculate(hours):
     results = []
-    for ngrams in range(1, 7): # 1-6
-        for threshold in numpy.arange(0.1, 1.1, 0.1): # 0.1-1.0
-            start_time = tm.time()
+    for name, calculation in calculations.items():
+        for ngrams in range(1, 7): # 1-6
+            for threshold in numpy.arange(0.1, 1.1, 0.1): # 0.1-1.0
+                start_time = tm.time()
 
-            cleaned = [(time, tweet, category, cleaner.clean(tweet)) for (time, tweet, category) in tweets]
-            tokenized = [(time, tweet, category, tokenizer.ngrams_tokenizer(cleaned_tweets, ngrams)) for (time, tweet, category, cleaned_tweets) in cleaned]
+                cleaned = [(time, tweet, category, cleaner.clean(tweet)) for (time, tweet, category) in tweets]
+                tokenized = [(time, tweet, category, tokenizer.ngrams_tokenizer(cleaned_tweets, ngrams)) for (time, tweet, category, cleaned_tweets) in cleaned]
 
-            distincts = []
-            tp, tn, fp, fn = 0, 0, 0, 0
+                distincts = []
+                tp, tn, fp, fn = 0, 0, 0, 0
 
-            for (time, tweet, category, tokens) in tokenized:
-                if len(distincts) == 0:
-                    distincts.append((time, tweet, tokens))
-                else:
-                    is_distinct = True
-                    for (distinct_time, distinct_tweet, distinct_tokens) in distincts:
-                        dt = datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
-                        distinct_dt = datetime.strptime(distinct_time, '%Y-%m-%d %H:%M:%S')
-                        time_diff = dt - distinct_dt
-
-                        if time_diff > timedelta(hours=hours):
-                            distincts.remove((distinct_time, distinct_tweet, distinct_tokens))
-                            continue
-
-                        index = algo.index(tokens, distinct_tokens)
-                        if index >= threshold:
-                            is_distinct = False
-                            break
-
-                    if is_distinct:
+                for (time, tweet, category, tokens) in tokenized:
+                    if len(distincts) == 0:
                         distincts.append((time, tweet, tokens))
-
-                        if category == 'new':
-                            tp += 1
-                        else:
-                            fp += 1
                     else:
-                        if category == 'new':
-                            fn += 1
+                        is_distinct = True
+                        for (distinct_time, distinct_tweet, distinct_tokens) in distincts:
+                            dt = datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
+                            distinct_dt = datetime.strptime(distinct_time, '%Y-%m-%d %H:%M:%S')
+                            time_diff = dt - distinct_dt
+
+                            if time_diff > timedelta(hours=hours):
+                                distincts.remove((distinct_time, distinct_tweet, distinct_tokens))
+                                continue
+
+                            index = calculation.index(tokens, distinct_tokens)
+                            if index >= threshold:
+                                is_distinct = False
+                                break
+
+                        if is_distinct:
+                            distincts.append((time, tweet, tokens))
+
+                            if category == 'new':
+                                tp += 1
+                            else:
+                                fp += 1
                         else:
-                            tn += 1
+                            if category == 'new':
+                                fn += 1
+                            else:
+                                tn += 1
 
-            time_elapsed = tm.time() - start_time
-            accuracy = (tp + tn) / (tp + tn + fp + fn)
-            precision = tp / (tp + fp)
-            recall = tp / (tp + fn)
-            fscore = 2 * (precision * recall) / (precision + recall)
+                time_elapsed = tm.time() - start_time
+                accuracy = (tp + tn) / (tp + tn + fp + fn)
+                precision = tp / (tp + fp)
+                recall = tp / (tp + fn)
+                fscore = 2 * (precision * recall) / (precision + recall)
 
-            print()
-            print('Limit hours: {}'.format(hours))
-            print('Ngrams: {}'.format(ngrams))
-            print('Threshold: {}'.format(threshold))
-            print('True positive: {}'.format(tp))
-            print('True negative: {}'.format(tn))
-            print('False positive: {}'.format(fp))
-            print('False negative: {}'.format(fn))
-            print('Accuracy: {}'.format(accuracy))
-            print('Precison: {}'.format(precision))
-            print('Recall: {}'.format(recall))
-            print('F-score: {}'.format(fscore))
-            print('Time elapsed: {}'.format(time_elapsed))
+                print()
+                print('Limit hours: {}'.format(hours))
+                print('Calculation: {}'.format(name))
+                print('Ngrams: {}'.format(ngrams))
+                print('Threshold: {}'.format(threshold))
+                print('True positive: {}'.format(tp))
+                print('True negative: {}'.format(tn))
+                print('False positive: {}'.format(fp))
+                print('False negative: {}'.format(fn))
+                print('Accuracy: {}'.format(accuracy))
+                print('Precison: {}'.format(precision))
+                print('Recall: {}'.format(recall))
+                print('F-score: {}'.format(fscore))
+                print('Time elapsed: {}'.format(time_elapsed))
 
-            results.append([hours, ngrams, threshold, tp, tn, fp, fn, accuracy, precision, recall, fscore, time_elapsed])
+                results.append([name, hours, ngrams, threshold, tp, tn, fp, fn, accuracy, precision, recall, fscore, time_elapsed])
     return results
 
 p = Pool(4)
-pool_results = p.map(calculate, range(12, 49, 12))
+pool_results = p.map(calculate, range(6, 49, 6))
 
 with open(os.path.join(os.path.dirname(__file__), args.output), 'w', newline='\n') as csv_output:
     csv_writer = csv.writer(csv_output, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-    csv_writer.writerow(['limit hours', 'ngrams', 'threshold', 'tp', 'tn', 'fp', 'fn', 'accuracy', 'precision', 'recall', 'f-score', 'time elapsed'])
+    csv_writer.writerow(['limit hours', 'calculation', 'ngrams', 'threshold', 'tp', 'tn', 'fp', 'fn', 'accuracy', 'precision', 'recall', 'f-score', 'time elapsed'])
     for pool_result in pool_results:
         for result in pool_result:
             csv_writer.writerow(result)
