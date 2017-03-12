964409b2f64e55b9452c7cba2c9d17f4338c5283
Dwi Aji Kurniawan <dwi.aji.kurniawan@gmail.com>
2017-03-12 16:36:48 +0700

    Averaging CV from n iterations

--- cv_ml_nb.py
+++ cv_ml_nb.py
@@ -9,6 +9,10 @@ from sklearn.svm import LinearSVC
 from sklearn.tree import DecisionTreeClassifier
 
 fold = 10
+n = 1000
+
+cv_accuracies = []
+cv_times = []
 
 with open(os.path.join(os.path.dirname(__file__), 'tweets_corpus/4095-pair-dataset-different.csv'), newline='\n') as csv_input:
     dataset = csv.reader(csv_input, delimiter=',', quotechar='"')
@@ -40,38 +44,49 @@ with open(os.path.join(os.path.dirname(__file__), 'tweets_corpus/4095-pair-datas
         'lcs': float(line[13]),
         }, line[4]) for line in dataset]
 
-random.shuffle(different_tweets)
+for i in range(n):
+    random.shuffle(different_tweets)
 
-tweets = different_tweets[:590] + similar_tweets * 5
+    tweets = different_tweets[:590] + similar_tweets * 5
 
-random.shuffle(tweets)
+    random.shuffle(tweets)
 
-print('Dataset of {} tweet-pairs'.format(len(tweets)))
+    print()
+    print('Dataset of {} tweet-pairs'.format(len(tweets)))
 
-accuracies = []
-times = []
+    accuracies = []
+    times = []
 
-for i in range(fold):
-    train_set = [features for features in tweets[0 : i * int(len(tweets) / fold)]] + \
-        [features for features in tweets[(i + 1) * int(len(tweets) / fold) : len(tweets)]]
-    test_set = [features for features in tweets[i * int(len(tweets) / fold) : (i + 1) * int(len(tweets) / fold)]]
+    for i in range(fold):
+        train_set = [features for features in tweets[0 : i * int(len(tweets) / fold)]] + \
+            [features for features in tweets[(i + 1) * int(len(tweets) / fold) : len(tweets)]]
+        test_set = [features for features in tweets[i * int(len(tweets) / fold) : (i + 1) * int(len(tweets) / fold)]]
 
-    print('\nIteration', (i + 1))
-    print('Training data:', len(train_set), 'data')
-    print('Test data:', len(test_set), 'data')
+        # print('\nIteration', (i + 1))
+        # print('Training data:', len(train_set), 'data')
+        # print('Test data:', len(test_set), 'data')
 
-    start_time = time.time()
-    # classifier = nltk.classify.SklearnClassifier(BernoulliNB()).train(train_set)
-    classifier = nltk.classify.SklearnClassifier(DecisionTreeClassifier()).train(train_set)
-    # classifier = nltk.classify.SklearnClassifier(LinearSVC()).train(train_set)
-    time_elapsed = round(time.time() - start_time, 2)
-    accuracy = nltk.classify.accuracy(classifier, test_set)
-    accuracies.append(accuracy)
-    times.append(time_elapsed)
+        start_time = time.time()
+        # classifier = nltk.classify.SklearnClassifier(BernoulliNB()).train(train_set)
+        classifier = nltk.classify.SklearnClassifier(DecisionTreeClassifier()).train(train_set)
+        # classifier = nltk.classify.SklearnClassifier(LinearSVC()).train(train_set)
+        time_elapsed = round(time.time() - start_time, 2)
+        accuracy = nltk.classify.accuracy(classifier, test_set)
+        accuracies.append(accuracy)
+        times.append(time_elapsed)
 
-    print('Accuracy: {}'.format(accuracy))
-    print('Time elapsed: {}'.format(time_elapsed))
+        # print('Accuracy: {}'.format(accuracy))
+        # print('Time elapsed: {}'.format(time_elapsed))
+
+    average_accuracy = sum(accuracies) / len(accuracies)
+    average_time = sum(times) / len(times)
+    cv_accuracies.append(average_accuracy)
+    cv_times.append(average_time)
+
+    print('Average accuracy: {}'.format(average_accuracy))
+    print('Average time elapsed: {}'.format(average_time))
 
 print()
-print('Average accuracy: {}'.format(sum(accuracies) / len(accuracies)))
-print('Average time elapsed: {}'.format(sum(times) / len(times)))
+print('Summary of {} iterations'.format(n))
+print('Average accuracy: {}'.format(sum(cv_accuracies) / len(cv_accuracies)))
+print('Average time elapsed: {}'.format(sum(cv_times) / len(cv_times)))
--- pair_threshold.py
+++ pair_threshold.py
@@ -8,7 +8,11 @@ parser.add_argument('-o', '--output', default='output.csv', help='File name for 
 args = parser.parse_args()
 
 fold = 10
+n = 1000
 calculations = ['cosine', 'dice', 'jaccard', 'manhattan', 'overlap', 'cosine_tfidf', 'dice_tfidf', 'manhattan_tfidf', 'lcs']
+
+cv_accuracies = []
+cv_times = []
 
 with open(os.path.join(os.path.dirname(__file__), 'tweets_corpus/4095-pair-dataset-different.csv'), newline='\n') as csv_input:
     dataset = csv.reader(csv_input, delimiter=',', quotechar='"')
@@ -40,52 +44,55 @@ with open(os.path.join(os.path.dirname(__file__), 'tweets_corpus/4095-pair-datas
         'lcs': float(line[13]),
         }, line[4]) for line in dataset]
 
-random.shuffle(different_tweets)
-
-tweets = different_tweets[:590] + similar_tweets * 5
-
-random.shuffle(tweets)
-
-print('Dataset of {} tweet-pairs'.format(len(tweets)))
-
-results = {}
-
 for threshold in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
-    result = {
-        'cosine': { 'true': 0, 'false': 0, 'accuracy': 0 },
-        'dice': { 'true': 0, 'false': 0, 'accuracy': 0 },
-        'jaccard': { 'true': 0, 'false': 0, 'accuracy': 0 },
-        'manhattan': { 'true': 0, 'false': 0, 'accuracy': 0 },
-        'overlap': { 'true': 0, 'false': 0, 'accuracy': 0 },
-        'cosine_tfidf': { 'true': 0, 'false': 0, 'accuracy': 0 },
-        'dice_tfidf': { 'true': 0, 'false': 0, 'accuracy': 0 },
-        'manhattan_tfidf': { 'true': 0, 'false': 0, 'accuracy': 0 },
-        'lcs': { 'true': 0, 'false': 0, 'accuracy': 0 },
+    accuracies = {
+        'cosine': [],
+        'dice': [],
+        'jaccard': [],
+        'manhattan': [],
+        'overlap': [],
+        'cosine_tfidf': [],
+        'dice_tfidf': [],
+        'manhattan_tfidf': [],
+        'lcs': [],
     }
 
-    for (similarity, category) in tweets:
-        for calculation in calculations:
-            if similarity[calculation] >= threshold:
-                if category == 'similar':
-                    result[calculation]['true'] += 1
+    for calculation in calculations:
+        for i in range(n):
+            random.shuffle(different_tweets)
+
+            tweets = different_tweets[:590] + similar_tweets * 5
+
+            random.shuffle(tweets)
+
+            print()
+            print('Dataset of {} tweet-pairs'.format(len(tweets)))
+
+            results = {}
+
+            true = 0
+            false = 0
+            for (similarity, category) in tweets:
+                if similarity[calculation] >= threshold:
+                    if category == 'similar':
+                        true += 1
+                    else:
+                        false += 1
                 else:
-                    result[calculation]['false'] += 1
-            else:
-                if category == 'different':
-                    result[calculation]['true'] += 1
-                else:
-                    result[calculation]['false'] += 1
+                    if category == 'different':
+                        true += 1
+                    else:
+                        false += 1
+            accuracies[calculation].append(true / (true + false))
 
     for calculation in calculations:
-        result[calculation]['accuracy'] = result[calculation]['true'] / (result[calculation]['true'] + result[calculation]['false'])
-
-    results[threshold] = result
+        accuracy = sum(accuracies[calculation]) / len(accuracies[calculation])
+        cv_accuracies.append((threshold, calculation, accuracy))
 
 
 with open(os.path.join(os.path.dirname(__file__), args.output), 'a', newline='\n') as csv_output:
     csv_writer = csv.writer(csv_output, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-    for t, res in results.items():
-        for c, r in res.items():
-            csv_writer.writerow([t, c, r['accuracy']])
-            print('{}\t{}\t{}'.format(t, c, r['accuracy']))
+    for (t, c, acc) in cv_accuracies:
+        csv_writer.writerow([t, c, acc])
+        print('{}\t{}\t{}'.format(t, c, acc))
             
